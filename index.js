024d4fbf942a6a4f37b914622caf3f22bb8b6bc9
aadityajoshi151 <aadityajoshi151@gmail.com>
2021-05-20 18:05:37 +0530

    titleytical started
    Hope it works on other one as well.

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("browsebtn").addEventListener("click",function(){
+    alert("Browse Button Pressed!")
+    document.getElementById("pathfield").value = "C:/Program Files/Titleytical"
+})
